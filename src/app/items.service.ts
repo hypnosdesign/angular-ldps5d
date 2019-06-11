73d8ec02eb95577af0e1251db6375fa5515ccf06
hypnosdesign <hypnos.design@gmail.com>
2019-06-12 00:00:30 +0200

    1 file modified

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Items} from './items';
 @Injectable()
 export class ItemsService {
+pathGithub = 'https://github.com/hypnosdesign/angular-ldps5d/raw/master/src/'
 
 cards: Items[] = [
     {
@@ -9,14 +10,14 @@ cards: Items[] = [
       titolo: 'card numero uno',
       sotto_titolo: 'sotto titolo card numero uno',
       descrizione: `Questa è una descrizione per la card numero uno`,
-      foto: 'assets/card-img-1.jpg',
+      foto: this.pathGithub +'assets/card-img-1.jpg',
       slide: [
-        'assets/card-img-1.jpg',
-        'assets/card-img-2.jpg',
-        'assets/card-img-3.jpg',
-        'assets/card-img-4.jpg',
-        'assets/card-img-5.jpg',
-        'assets/card-img-6.jpg'
+        this.pathGithub +'assets/card-img-1.jpg',
+        this.pathGithub +'assets/card-img-2.jpg',
+        this.pathGithub +'assets/card-img-3.jpg',
+        this.pathGithub +'assets/card-img-4.jpg',
+        this.pathGithub +'assets/card-img-5.jpg',
+        this.pathGithub +'assets/card-img-6.jpg'
       ]
     },
     {
@@ -24,18 +25,18 @@ cards: Items[] = [
       titolo: 'card numero due',
       sotto_titolo: 'sotto titolo card numero due',
       descrizione: `Questa è una descrizione per la card numero due`,
-      foto: 'assets/card-img-7.jpg'
+      foto: this.pathGithub +'assets/card-img-7.jpg'
     },
     {
       id: 3,
       titolo: 'card numero tre',
       sotto_titolo: 'sotto titolo card numero tre',
       descrizione: `Questa è una descrizione per la card numero tre`,
-      foto: 'assets/card-img-9.jpg',
+      foto: this.pathGithub +'assets/card-img-9.jpg',
       slide: [
-        'assets/card-img-9.jpg',
-        'assets/card-img-10.jpg',
-        'assets/card-img-11.jpg'
+        this.pathGithub +'assets/card-img-9.jpg',
+        this.pathGithub +'assets/card-img-10.jpg',
+        this.pathGithub +'assets/card-img-11.jpg'
       ]
     },
     {
@@ -43,7 +44,7 @@ cards: Items[] = [
       titolo: 'card numero quattro',
       sotto_titolo: 'sotto titolo card numero quattro',
       descrizione: `Questa è una descrizione per la card numero quattro`,
-      foto: 'assets/card-img-8.jpg'
+      foto: this.pathGithub +'assets/card-img-8.jpg'
     }
 
   ];
